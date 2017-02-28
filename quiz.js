68f1b2b8707d2ff395cb1de664361760b48fa88d
ConorS93 <conorsweetingham1993@hotmail.co.uk>
2017-02-28 18:24:54 +0000

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
                   
-  $('.questionForm').hide();
+  $('.questionSetup').hide();
 
   $('#q1').show();
 
 $('#q1 #submit').click(function(){
-  $('.questionForm').hide();
+  $('.questionSetup').hide();
   $('#q2').fadeIn(250);
   return false;
   })
