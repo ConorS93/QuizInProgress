acf2704bafbd7766e3c53c96ecc90d6fd077bc11
ConorS93 <conorsweetingham1993@hotmail.co.uk>
2017-02-28 18:10:33 +0000

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,9 @@
 $(document).ready(function()){
   $('.questionForm').hide();
+  $('#q1').show();
+$('#q1 #submit').click(function(){
+  $('.questionForm').hide();
+  $('#q2').show();
+  return false;
+  }
 }
