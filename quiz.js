b12edb8f938272d15ec4a10c5070e419c7f6dabb
ConorS93 <conorsweetingham1993@hotmail.co.uk>
2017-02-28 20:49:54 +0000

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -46,37 +46,6 @@ $('#q1 #submit').click(function(){
   return false;
   })
 })
-
-function process(q){
- if(q == 'q1){
-      var submitted = $('input [name=q1]:checked').val();
-      if (submitted == sessionStorage.a1){
-        score++;
-      }
-    }
-if(q == 'q2){
-      var submitted = $('input [name=q2]:checked').val();
-      if (submitted == sessionStorage.a2){
-        score++;
-      }
-    }
-    if(q == 'q3){
-      var submitted = $('input [name=q3]:checked').val();
-      if (submitted == sessionStorage.a3){
-        score++;
-      }
-    }
-    if(q == 'q4){
-      var submitted = $('input [name=q4]:checked').val();
-      if (submitted == sessionStorage.a4){
-        score++;
-      }
-    }
-    if(q == 'q5){
-      var submitted = $('input [name=q5]:checked').val();
-      if (submitted == sessionStorage.a5){
-        score++;
-      }
       $('#results').html ('<h3> Your score is: '+score+' out of 10 </h3><a href="page2.html"> take quiz again</a>');
     }
     return false;
